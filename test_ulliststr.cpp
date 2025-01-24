e65fa1e2e8deddb76129cbbeaf61d2e9a12b7d5f
Evan Adami <eadami@usc.edu>
2025-01-23 21:15:26 -0800

    Completed unrolled linked list

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -7,7 +7,63 @@
 
 //Use this file to test your ulliststr implementation before running the test suite
 
+using namespace std;
+
 int main(int argc, char* argv[])
 {
+   ULListStr list;
+   
+   cout << list.size() << endl;
+   
+   list.push_back("a");
 
+   cout << list.front() << endl;
+   cout << list.back() << endl;
+   cout << list.size() << endl;
+   
+   list.push_back("b");
+   list.push_back("c");
+
+   cout << list.get(0) << endl;
+   cout << list.get(1) << endl;
+   cout << list.get(2) << endl;
+   
+   list.push_front("z");
+   list.push_front("y");
+
+   cout << list.front() << endl;
+   cout << list.get(1) << endl;
+   cout << list.back() << endl;
+   cout << list.size() << endl;
+   
+   list.pop_back();
+   list.pop_back();
+
+   cout << list.size() << endl;
+   cout << list.back() << endl;
+   
+   list.pop_front();
+
+   cout << list.front() << endl;
+   cout << list.size() << endl;
+   
+   list.pop_front();
+   list.pop_front();
+
+   cout << list.size() << endl;
+   
+   list.push_front("new front");
+   list.push_back("new back");
+
+   cout << list.front() << endl;
+   cout << list.back() << endl;
+   cout << list.size() << endl;
+   
+   for(int i = 0; i < 12; i++) {
+       list.push_back("hello");
+   }
+   
+   cout << list.size() << endl;
+   cout << list.front() << endl;
+   cout << list.back() << endl;
 }
